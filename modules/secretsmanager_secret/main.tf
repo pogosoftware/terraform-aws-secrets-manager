9897272166d7aec5b1472343cc33d1684e37a505
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2022-04-22 13:45:31 +0200

    Added secretsmanager_secret module

---
+++ modules/secretsmanager_secret/main.tf
@@ -0,0 +1,22 @@
+resource "aws_secretsmanager_secret" "this" {
+  count = var.create ? 1 : 0
+
+  name                           = local.name
+  name_prefix                    = local.name_prefix
+  description                    = var.description
+  kms_key_id                     = var.kms_key_id
+  policy                         = var.policy
+  recovery_window_in_days        = var.recovery_window_in_days
+  force_overwrite_replica_secret = var.force_overwrite_replica_secret
+
+  dynamic "replica" {
+    for_each = var.replica
+
+    content {
+      kms_key_id = lookup(each.value, "kms_key_id", null)
+      region     = each.value.region
+    }
+  }
+
+  tags = merge({ "Name" = var.name }, var.tags)
+}
